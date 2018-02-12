32cb3fc6f6b4f4fbdeefb5cbd66523c5ea05a9f4
Fekih Ahmed <fekihahm@informatique.univ-paris-diderot.fr>
2018-02-12 15:18:32 +0100

    exo1

---
+++ TP01/src/MyThread2.java
@@ -0,0 +1,20 @@
+
+public class MyThread2 extends Thread {
+	public MonObjet o;
+	public int nbwrite;
+
+	public MyThread2(MonObjet o, int nbwrite) {
+		this.o = o;
+		this.nbwrite = nbwrite;
+	}
+
+	public void run() {
+		// System.out.println("Je suis la thread " +ThreadID.get());
+		for (int i = 0; i < nbwrite; i++) {
+			o.add();
+			this.yield();
+		}
+		// System.out.println("la thread "+ThreadID.get()+
+		System.out.println("value" + o.value + ", " + "valuebis" + o.valuebis + " et " + " last " + o.last.get());
+	}
+}
